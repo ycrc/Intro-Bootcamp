62a3e40b2a6b6c0d239ea62edebae0849208172f
Robert Bjornson <rbjornson@gmail.com>
2018-01-09 16:51:28 -0500

    removed old LSF and Torque examples

--- Examples/LSF/myscript.R
+++
@@ -1,5 +0,0 @@
-pi=3.14
-Sys.sleep(15)
-print ("hello world")
-print (pi)
-
--- Examples/Torque/array.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#PBS -t 1-10
-#PBS -m abe -M robert.bjornson@yale.edu
-
-cd $PBS_O_WORKDIR
-
-sleep 20; echo "task ${PBS_ARRAYID} $(date)" > TARRAY_${PBS_ARRAYID}.out
-
