0cd1fbeee7f79aefeb44b0d6e024c5564066a88f
Robert Bjornson <rbjornson@gmail.com>
2018-01-10 14:28:23 -0500

    modified dSQ example

---
+++ Examples/Slurm/mkjobs.py
@@ -0,0 +1,9 @@
+
+with open('bwajobs.txt', 'w') as fp:
+    for i in range(20):
+        fp.write("module load SAMtools BWA; bwa mem BWAIndex/genome.fa Data/reads%(i)02d.fa > Results/out%(i)02d.sam; samtools sort -O bam -o Results/out%(i)02d.bam Results/out%(i)02d.sam\n" % locals())
+
+with open('bwajobsbetter.txt', 'w') as fp:
+    for i in range(20):
+        fp.write("module load SAMtools BWA; bwa mem BWAIndex/genome.fa Data/reads%(i)02d.fa | samtools sort -O bam -o Results/out%(i)02d.bam -\n" % locals())
+
