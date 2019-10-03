597ef080c24813b41f5b8114c16b55e82e447e55
Robert Bjornson <rbjornson@gmail.com>
2019-10-03 15:13:56 -0400

    small changes to array.sh

--- Examples/Slurm/array.sh
+++ Examples/Slurm/array.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#SBATCH --array=1-10 
+#SBATCH --array=1-20%10
 #SBATCH -t 3:00
 #SBATCH --mail-type=ALL
 
+module purge
 module load R
 Rscript arrscript.R ${SLURM_ARRAY_TASK_ID} > results_${SLURM_ARRAY_TASK_ID}.out
